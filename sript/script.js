f64efec47d715c9a0d6807183b65c541984262ce
Andrei Soroca <sorocandrei@yandex.ru>
2020-11-24 23:05:06 +0200

    The first commit

---
+++ sript/script.js
@@ -0,0 +1,19 @@
+console.log(3);
+alert('Hello!');
+console.log('Hello world!');
+
+//var let const
+
+
+let money = 'money';
+console.log('money: ', money);
+
+let income = 'income';
+var addExpenses = 33;
+const deposit = 100;
+let mission = 'mission';
+var period = 'period';
+
+
+let agePerson = 20;
+console.log('agePerson: ', agePerson);
