7a7a4512758b5cfb416b745b6dea5f38644bfd88
Andrei Soroca <sorocandrei@yandex.ru>
2020-11-26 21:00:38 +0200

    Modificate the main

--- sript/script.js
+++ sript/script.js
@@ -1,19 +1,3 @@
 console.log(1);
 alert('Hello!');
 console.log('Hello world!');
-
-//var let const
-
-
-let money = 'money';
-console.log('money: ', money);
-
-let income = 'income';
-var addExpenses = 33;
-const deposit = 100;
-let mission = 'mission';
-var period = 'period';
-
-
-let agePerson = 20;
-console.log('agePerson: ', agePerson);
